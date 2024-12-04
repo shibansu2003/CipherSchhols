1e6f7ae69f407870c7a208bf0b44105738540cbf
SHIBANSU MANDAL <shibansumandal2003@gmail.com>
2024-12-04 23:02:20 +0530

    Concatenated Strings

---
+++ ConcatenateStrings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string s,q;
+    cout<<"Enter your first String: "<<endl;;
+    cin>>s;
+    cout<<"Enter your second String: "<<endl;;
+    cin>>q;
+    cout<<"The concatenated String is:  "<<s+q<<endl;
+    return 0;
+}
